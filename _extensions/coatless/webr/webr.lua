35bc27aa0d7af99191d927759e0abfd98e4961c6
W. Joel Schneider <w.joel.schneider@gmail.com>
2024-03-27 06:59:01 -0400

    update quarto-webr

--- _extensions/coatless/webr/webr.lua
+++ _extensions/coatless/webr/webr.lua
@@ -12,7 +12,7 @@ local hasDoneWebRSetup = false
 -- https://docs.r-wasm.org/webr/latest/api/js/interfaces/WebR.WebROptions.html
 
 -- Define a base compatibile version
-local baseVersionWebR = "0.2.2"
+local baseVersionWebR = "0.3.1"
 
 -- Define where WebR can be found
 local baseUrl = "https://webr.r-wasm.org/v".. baseVersionWebR .."/"
@@ -65,6 +65,7 @@ local qwebRDefaultCellOptions = {
   ["comment"] = "",
   ["label"] = "",
   ["autorun"] = "",
+  ["read-only"] = "false",
   ["classes"] = "",
   ["dpi"] = 72,
   ["fig-cap"] = "",
@@ -400,8 +401,8 @@ local function ensureWebRSetup()
   -- Embed Support Files to Avoid Resource Registration Issues
   -- Note: We're not able to use embed-resources due to the web assembly binary and the potential for additional service worker files.
   quarto.doc.include_text("in-header", [[
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs/editor/editor.main.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/monaco-editor@0.47.0/min/vs/editor/editor.main.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
   ]])
 
   -- Insert the extension styling for defined elements
